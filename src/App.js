ac85ef64d2085a4dbdcd0471966fa811873b558d
eujuju888 <eujuju@yahoo.com>
2025-03-14 23:10:17 -0700

    Initial commit - Upload Slay Like a Queen MVP

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Slay Like a Queen 👑</h1>
+      <p>Welcome to your badass affirmation app!</p>
+    </div>
+  );
+}
+
+export default App;
